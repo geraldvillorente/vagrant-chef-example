b35991202a91d4e3d68631ea3d08d8ccb37f8430
Pete Hodgson <git@thepete.net>
2013-07-28 13:53:00 -0700

    use a template rather than a cookbook_file

--- site-cookbooks/my-first-server/recipes/default.rb
+++ site-cookbooks/my-first-server/recipes/default.rb
@@ -1,9 +1,11 @@
 include_recipe "apache2"
 apache_site 'default'
 
-cookbook_file "index.html" do
+template "index.html" do
   path "/var/www/index.html"
 
   owner node['apache']['user']
   group node['apache']['group']
+
+  variables( :title => "Hello from a chef template" )
 end
