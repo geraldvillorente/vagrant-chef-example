bf12748ab4831186c989a8ceceed2246f382d12a
Pete Hodgson <git@thepete.net>
2013-07-28 13:42:54 -0700

    Add new index.html to default site

--- site-cookbooks/my-first-server/recipes/default.rb
+++ site-cookbooks/my-first-server/recipes/default.rb
@@ -1,2 +1,9 @@
 include_recipe "apache2"
 apache_site 'default'
+
+cookbook_file "index.html" do
+  path "/var/www/index.html"
+
+  owner node['apache']['user']
+  group node['apache']['group']
+end
